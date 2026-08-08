afdfe8213e66e0a8f972d26e5a11a10f1eb7d77c
Pacey0216 <paveguteza@gmail.com>
2026-08-09 00:51:37 +0800

    Configure Top Watch 102 sales contacts

--- config.js
+++ config.js
@@ -3,8 +3,15 @@ window.STORE_CONFIG = {
   "apiUrl": "https://script.google.com/macros/s/AKfycbx6ys80ltiEvWuzOCskTR7vocWm9nNvEDQPDbojwzIA3j99G74DM3Wz6OPt4y4h9A/exec",
   "sheetReference": "https://docs.google.com/spreadsheets/d/1ljK2RqkdA8E3iEpEutrhMnTurxGqXwY6Kb8yF2hW3S8/edit?gid=1807963011#gid=1807963011",
   "storeFilter": "topwatch102",
-  "messengerUrl": "https://m.me/YOUR_TOP_WATCH_102_USERNAME",
+  "messengerUrl": "https://m.me/61587432892689",
+  "facebookUrl": "https://www.facebook.com/profile.php?id=61587432892689",
+  "phoneDisplay": "0906 160 6316",
+  "phoneUrl": "tel:+639061606316",
+  "email": "kidaweeda977@yahoo.com",
+  "whatsappDisplay": "+63 906 160 6316",
+  "whatsappUrl": "https://wa.me/639061606316",
   "currency": "PHP",
   "locale": "en-PH",
+  "refreshIntervalMs": 60000,
   "sampleMode": false
 };
