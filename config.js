dbeb3fd0f3f22e22a94d58caaff0249713bec6df
Pacey0216 <paveguteza@gmail.com>
2026-08-08 23:47:56 +0800

    Connect Top Watch 102 to live inventory API

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.STORE_CONFIG = {
   "brandName": "Top Watch 102",
-  "apiUrl": "",
+  "apiUrl": "https://script.google.com/macros/s/AKfycbx6ys80ltiEvWuz0CskTR7vocWm9nNvEDQPDbojwzlA3j99G74DM3Wz6OPt4y4h9A/exec",
   "sheetReference": "https://docs.google.com/spreadsheets/d/1ljK2RqkdA8E3iEpEutrhMnTurxGqXwY6Kb8yF2hW3S8/edit?gid=1807963011#gid=1807963011",
   "storeFilter": "topwatch102",
   "messengerUrl": "https://m.me/YOUR_TOP_WATCH_102_USERNAME",
