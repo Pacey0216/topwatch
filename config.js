4669ef7a3fd2b50056525cc3d6fd3a42e0073ab7
Pacey0216 <paveguteza@gmail.com>
2026-08-08 23:36:40 +0800

    Correct Top Watch 102 config and Products sheet reference

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window.STORE_CONFIG = {
-  "brandName": "Top Watch 101",
+  "brandName": "Top Watch 102",
   "apiUrl": "",
-  "sheetReference": "https://docs.google.com/spreadsheets/d/1ljK2RqkdA8E3iEpEutrhMnTurxGqXwY6Kb8yF2hW3S8/edit?gid=537048309#gid=537048309",
-  "storeFilter": "topwatch101",
-  "messengerUrl": "https://m.me/YOUR_TOP_WATCH_101_USERNAME",
+  "sheetReference": "https://docs.google.com/spreadsheets/d/1ljK2RqkdA8E3iEpEutrhMnTurxGqXwY6Kb8yF2hW3S8/edit?gid=1807963011#gid=1807963011",
+  "storeFilter": "topwatch102",
+  "messengerUrl": "https://m.me/YOUR_TOP_WATCH_102_USERNAME",
   "currency": "PHP",
   "locale": "en-PH",
-  "sampleMode": true
+  "sampleMode": false
 };
